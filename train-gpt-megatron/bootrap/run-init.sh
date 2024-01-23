d4c70286722f385f56ec9b5b08d08de3afbd4a09
Nika Wang <danielwzhg@gmail.com>
2024-01-23 05:46:48 +0000

    WIP

--- train-gpt-megatron/bootrap/run-init.sh
+++ train-gpt-megatron/bootrap/run-init.sh
@@ -8,17 +8,20 @@ git clone https://github.com/nikawang/Megatron-DeepSpeed.git
 cd Megatron-DeepSpeed
 python ./setup.py install
 
+cd dataset
 wget https://huggingface.co/bigscience/misc-test-data/resolve/main/stas/oscar-1GB.jsonl.xz
 wget https://huggingface.co/gpt2/resolve/main/vocab.json?download=true -O gpt2-vocab.json
 wget https://huggingface.co/gpt2/resolve/main/merges.txt?download=true -O gpt2-merges.txt
 xz -d oscar-1GB.jsonl.xz
 
-
+cd -
 python tools/preprocess_data.py \
-       --input oscar-1GB.jsonl \
+       --input ./dataset/oscar-1GB.jsonl \
        --output-prefix my-gpt2 \
-       --vocab-file gpt2-vocab.json \
+       --vocab-file ./dataset/gpt2-vocab.json \
        --dataset-impl mmap \
        --tokenizer-type GPT2BPETokenizer \
-       --merge-file gpt2-merges.txt \
+       --merge-file ./dataset/gpt2-merges.txt \
        --append-eod --workers 8
+
+cp my-gpt2* ./dataset
